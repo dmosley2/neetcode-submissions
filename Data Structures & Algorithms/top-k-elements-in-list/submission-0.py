cfd5ee489e61f1f33a484942d4160bf2dce1dce7
Dominique Mosley <114778677+dmosley2@users.noreply.github.com>
2026-07-07 23:01:39 -0700

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        array_of_lists = [[] for i in range(len(nums) + 1)]
+        results = []
+        #key is number, value is frequency
+        hash_map = {}
+        for num in nums:
+            if not num in hash_map:
+                hash_map[num] = 1
+            else:
+                hash_map[num] += 1
+        for key in hash_map.keys():
+            val = hash_map.get(key)
+            array_of_lists[val].append(key)
+        for i in range(len(array_of_lists) - 1, 0, -1):
+            for j in range(len(array_of_lists[i])):
+                if len(results) < k:
+                    results.append(array_of_lists[i][j])
+        return results
+
+
+        
